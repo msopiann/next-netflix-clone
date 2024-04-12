c1b58f80dd7a1dc0a790904c223eb694f903a11d
msopiann <muhammadsopian452@gmail.com>
2024-04-12 17:11:59 +0700

    style: add margin bottom in footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="mb-4">
       <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="footer-logo">
