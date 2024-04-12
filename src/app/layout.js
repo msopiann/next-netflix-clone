3870a26bbee43e23249d90e18bd57f8711435f7b
msopiann <muhammadsopian452@gmail.com>
2024-04-12 18:08:30 +0700

    chore: change row fetchurl

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={poppins.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
           title="Netflix Originals"
           fetchUrl={requests.fetchNeflixOriginals}
         />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Popular Movies" fetchUrl={requests.fetchPopularMovies} />
         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
